a54a8f607d5c9d3a6d12b3d1da75f312beb7599a
Manux03 <ma.ariast@duocuc.cl>
2021-06-05 23:48:52 -0400

    Actualización
    Actualización de pagina

--- nav.js
+++ nav.js
@@ -1,13 +1,16 @@
 
 $(document).ready(function(){
+  $(".black").css("background" , "rgba(255, 255, 255, 0.0)");
+  $(".black").css("background" , "rgba(0, 0, 0, 0.3)");
+  $(".black").css ("transition","0.5s","ease");
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 100) {
         $(".black").css ("transition","0.5s","ease");
-	    $(".black").css("background" , "rgba(0, 0, 0, 0.5)");
-	  }
-      else{
-        $(".black").css("background" , "rgba(255, 255, 255, 0.0)");
-      }
+	    $(".black").css("background" , "rgba(0, 0, 0, 0.7)");
+	  }else{
+      $(".black").css("background" , "rgba(0, 0, 0, 0.3)");
+      $(".black").css ("transition","0.5s","ease");
+    }
   })
 })
